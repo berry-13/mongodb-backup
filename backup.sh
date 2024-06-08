425a19fa5e04bbe81fc1122931ef4e56766705e8
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2024-06-09 00:17:35 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,4 +8,8 @@ mkdir -p $BACKUP_DIR
 
 mongodump --uri "${MONGO_URI}" --out $BACKUP_DIR/$DATE
 
-find $BACKUP_DIR -type d -mtime +7 -exec rm -rf {} \;
+tar -czf $BACKUP_DIR/$DATE.tar.gz $BACKUP_DIR/$DATE
+
+rm -rf $BACKUP_DIR/$DATE
+
+find $BACKUP_DIR -type f -name '*.tar.gz' -mtime +7 -exec rm {} \;
